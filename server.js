67a102303bcc0aef53b9d25f9f2e731806a880fe
aywory <153110264+aywory@users.noreply.github.com>
2026-03-14 11:28:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http, { cors: { origin: "*" } });
+const cors = require("cors"); // Добавили модуль cors
+
+// Разрешаем все подключения со всех сайтов
+app.use(cors());
+
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*", // Разрешить всем
+    methods: ["GET", "POST"],
+  },
+});
 
 io.on("connection", (socket) => {
   console.log("Пользователь подключился");
 
-  // Присоединение к комнате
   socket.on("joinRoom", (roomId) => {
     socket.join(roomId);
+    console.log("Зашел в комнату: " + roomId);
   });
 
-  // Передача команд плеера (пауза, плей, перемотка)
   socket.on("playerEvent", (data) => {
-    // Отправляем всем в комнате, кроме отправителя
     socket.to(data.roomId).emit("playerEvent", data);
   });
 
-  // Чат
   socket.on("message", (data) => {
     io.to(data.roomId).emit("message", data);
   });
 });
 
-http.listen(3000, () => console.log("Сервер запущен на порту 3000"));
+// Важно: порт должен быть таким, какой дает Render
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => console.log("Сервер запущен на порту " + PORT));
