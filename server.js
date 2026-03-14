c0619e154e1230eb871f2dc5dc7af0c668443547
aywory <153110264+aywory@users.noreply.github.com>
2026-03-14 11:34:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,56 @@
-const express = require("express");
-const app = express();
-const http = require("http").createServer(app);
-const cors = require("cors"); // Добавили модуль cors
+function loadVideo() {
+  const link = document.getElementById("vkLink").value;
+  // Исправленное регулярное выражение, которое понимает и m.vkvideo.ru и vk.com
+  const match = link.match(/video(-?\d+_\d+)/);
 
-// Разрешаем все подключения со всех сайтов
-app.use(cors());
+  if (match) {
+    const videoId = match[1]; // Получим что-то вроде -56169357_456266703
+    console.log("ID видео найден:", videoId);
+    initPlayer(videoId);
 
-const io = require("socket.io")(http, {
-  cors: {
-    origin: "*", // Разрешить всем
-    methods: ["GET", "POST"],
-  },
+    // Отправляем всем в комнате, что мы сменили видео
+    socket.emit("playerEvent", {
+      roomId: roomId,
+      action: "changeVideo",
+      videoId: videoId,
+    });
+  } else {
+    alert(
+      "Неверная ссылка на видео ВК. Убедитесь, что в ссылке есть слово 'video' и цифры.",
+    );
+  }
+}
+
+function initPlayer(videoId) {
+  const parts = videoId.split("_");
+  const oid = parts[0];
+  const vid = parts[1];
+
+  const container = document.getElementById("vk_player");
+  container.innerHTML = ""; // Очищаем старое видео
+
+  // Создаем плеер
+  player = new VK.VideoPlayer(container, {
+    oid: oid,
+    id: vid,
+    width: "100%", // Растянуть на всю ширину
+    height: 450,
+    autoplay: 1,
+  });
+
+  // Слушаем события (когда плеер будет готов)
+  player.on("inited", () => {
+    console.log("Плеер готов к работе");
+  });
+}
+
+// Добавим в обработчик socket.on('playerEvent') поддержку смены видео:
+socket.on("playerEvent", (data) => {
+  if (data.action === "changeVideo") {
+    initPlayer(data.videoId);
+  }
+  if (player) {
+    if (data.action === "play") player.play();
+    if (data.action === "pause") player.pause();
+  }
 });
-
-io.on("connection", (socket) => {
-  console.log("Пользователь подключился");
-
-  socket.on("joinRoom", (roomId) => {
-    socket.join(roomId);
-    console.log("Зашел в комнату: " + roomId);
-  });
-
-  socket.on("playerEvent", (data) => {
-    socket.to(data.roomId).emit("playerEvent", data);
-  });
-
-  socket.on("message", (data) => {
-    io.to(data.roomId).emit("message", data);
-  });
-});
-
-// Важно: порт должен быть таким, какой дает Render
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log("Сервер запущен на порту " + PORT));
